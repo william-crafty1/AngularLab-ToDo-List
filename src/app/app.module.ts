c781fb51b6ccd6801f312132df654aeaf4658740
will c <will.cra@outlook.com>
2020-07-01 18:30:23 -0400

    fixed module file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { NgModule } from '@angular/core';
 import { ToDoComponent} from './components/toDo/toDo.component';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ToDoComponent,
-    NavbarComponent
   ],
   imports: [
     BrowserModule,
